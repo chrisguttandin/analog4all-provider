d1798f9d8a59bf17bacac1c347526bcbc28ee276
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-10 13:42:51 +0200

    update RTCIceServer dictionaries

--- app/scripts/modules/instruments/services/peer-connecting.js
+++ app/scripts/modules/instruments/services/peer-connecting.js
@@ -2,17 +2,13 @@ import { Observable } from 'rxjs/Observable';
 import { filter } from 'rxjs/operator/filter';
 
 /* eslint-disable indent */
-const ICE_SERVERS = [{
-          url: 'stun:stun.l.google.com:19302'
-      }, {
-          url: 'stun:stun1.l.google.com:19302'
-      }, {
-          url: 'stun:stun2.l.google.com:19302'
-      }, {
-          url: 'stun:stun3.l.google.com:19302'
-      }, {
-          url: 'stun:stun4.l.google.com:19302'
-      }];
+const ICE_SERVERS = [ { urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302'
+      ] } ];
 /* eslint-enable indent */
 
 export class PeerConnectingService {
