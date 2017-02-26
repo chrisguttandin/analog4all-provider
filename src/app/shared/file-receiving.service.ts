c794208ecad31ea1eb787db9146730ca24abbe9d
chrisguttandin <chrisguttandin@media-codings.com>
2017-02-26 01:46:51 +0100

    add missing return type

--- src/app/shared/file-receiving.service.ts
+++ src/app/shared/file-receiving.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FileReceivingService {
 
-    public receive (dataChannelSubject) {
+    public receive (dataChannelSubject): Promise<ArrayBuffer> {
         return new Promise((resolve, reject) => {
             let buffer;
 
