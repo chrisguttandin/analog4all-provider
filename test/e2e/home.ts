4ee6bd9139306db2d7aa8422aeab33bde18e1ea9
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-30 19:40:41 +0200

    fix e2e test

--- test/e2e/home.ts
+++ test/e2e/home.ts
@@ -10,7 +10,7 @@ describe('/', () => {
 
     it('should display the correct headline', () => {
         page.navigateTo();
-        expect(page.getHeadline()).toEqual('Analog4All Provider');
+        expect<any>(page.getHeadline()).toEqual('Analog4All Provider');
     });
 
 });
