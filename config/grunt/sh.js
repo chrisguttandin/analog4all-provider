074fd8df0687569082c851c30973509fb899fc1c
chrisguttandin <chrisguttandin@media-codings.com>
2017-10-10 19:49:13 +0200

    chore(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,6 +1,6 @@
 module.exports = {
     analyze: {
-        cmd: 'ng build --aot --prod --sourcemaps --stats-json && (bundle-buddy build/*.map || webpack-bundle-analyzer build/stats.json)'
+        cmd: 'ng build --aot --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
     },
     build: {
         cmd: 'ng build --aot --base-href /analog4all-provider --no-sourcemap --prod'
