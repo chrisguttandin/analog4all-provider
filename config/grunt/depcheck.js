b64982970a46df20aa656d18f3f2767f81e1dd68
chrisguttandin <chrisguttandin@media-codings.com>
2017-11-12 01:11:10 +0100

    run sonar as part of the smoke test

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -12,6 +12,7 @@ module.exports = {
                 '@angular/cli',
                 '@angular/compiler-cli',
                 '@angular/language-service',
+                '@sonarwhal/sonar',
                 '@types/*',
                 'axe-core',
                 'bundle-buddy',
