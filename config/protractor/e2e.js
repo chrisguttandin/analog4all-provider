a847ce2322ceb2680c0209356180f7a32dee13ac
chrisguttandin <chrisguttandin@media-codings.com>
2018-03-05 04:26:13 +0100

    build(config): fix protractor config

--- config/protractor/e2e.js
+++ config/protractor/e2e.js
@@ -10,8 +10,8 @@ exports.config = {
         chromeOptions: {
             // @todo Running the tests in headless mode seems to disable the Web MIDI API.
             args: (process.env.TRAVIS) ?
-                [ '--device-scale-factor=2', '--disable-gpu', '--force-device-scale-factor=2', '--window-size=1024,768' ] :
-                [ '--device-scale-factor=2', '--disable-gpu', '--force-device-scale-factor=2', '--headless', '--window-size=1024,768' ]
+                [ '--device-scale-factor=2', '--disable-gpu', '--force-device-scale-factor=2', '--headless', '--window-size=1024,768' ] :
+                [ '--device-scale-factor=2', '--disable-gpu', '--force-device-scale-factor=2', '--window-size=1024,768' ]
         }
     },
 
