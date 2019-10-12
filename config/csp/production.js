42316a2c5fc192f9d6099a95f63944075c0bbf2c
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-10-12 16:17:14 +0200

    fix: add eu-west-1.amazonaws.com to connect-src directive

--- config/csp/production.js
+++ config/csp/production.js
@@ -6,6 +6,7 @@ module.exports = {
             'blob:',
             "'self'",
             'https://analog4all-samples.s3.amazonaws.com',
+            'https://analog4all-samples.s3.eu-west-1.amazonaws.com',
             'https://jbnw79pt56.execute-api.eu-west-1.amazonaws.com',
             'wss://sock.cat'
         ],
