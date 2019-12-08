a16b1b73a0e3d4b37a3a2fe0001d377138834c7c
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-08 19:10:51 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -43,8 +43,8 @@ module.exports = (grunt) => {
         },
         'smoke': {
             cmd: (env.TRAVIS) ?
-                "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint --tracking=off https://chrisguttandin.github.io/analog4all-provider" :
-                "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint --tracking=off https://chrisguttandin.github.io/analog4all-provider"
+                "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint --telemetry=off https://chrisguttandin.github.io/analog4all-provider" :
+                "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint --telemetry=off https://chrisguttandin.github.io/analog4all-provider"
         },
         'test': {
             cmd: 'ng test --watch false'
