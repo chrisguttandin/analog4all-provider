130a81a1b2586571195c5ab9e526b7a1fe2dc76e
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-26 10:39:47 +0200

    fix: include all actions in TStoreAction type

--- src/app/store/types/store-action.ts
+++ src/app/store/types/store-action.ts
@@ -1,14 +1,28 @@
 import {
     IAddInstrumentAction,
+    IDeleteInstrumentAction,
+    IDeleteInstrumentFailAction,
+    IDeleteInstrumentSuccessAction,
     IMergeMidiConnectionsAction,
+    IPatchInstrumentAction,
+    IPatchInstrumentFailAction,
+    IPatchInstrumentSuccessAction,
     IRemoveInstrumentAction,
     IUpdateInstrumentAction,
-    IUpdateMidiConnectionAction
+    IUpdateMidiConnectionAction,
+    IWatchMidiOutputsAction
 } from '../interfaces';
 
 export type TStoreAction =
     | IAddInstrumentAction
+    | IDeleteInstrumentAction
+    | IDeleteInstrumentFailAction
+    | IDeleteInstrumentSuccessAction
     | IMergeMidiConnectionsAction
+    | IPatchInstrumentAction
+    | IPatchInstrumentFailAction
+    | IPatchInstrumentSuccessAction
     | IRemoveInstrumentAction
     | IUpdateInstrumentAction
-    | IUpdateMidiConnectionAction;
+    | IUpdateMidiConnectionAction
+    | IWatchMidiOutputsAction;
