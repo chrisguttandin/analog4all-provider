2b0bf683057830e1deea5c1194dac2ac26eb4d18
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-01-04 10:37:49 +0100

    fix: update PermissionDescriptor for midi to support sysex

--- src/permissions.d.ts
+++ src/permissions.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-unused-vars
 interface Permissions {
     // @todo TypeScript v4.4.2 removed a few properties from the PermissionDescriptor interface.
-    query(permissionDesc: { name: 'microphone' | 'midi' | PermissionName }): Promise<PermissionStatus>;
+    query(permissionDesc: { name: 'microphone' | PermissionName } | { name: 'midi'; sysex?: boolean }): Promise<PermissionStatus>;
 }
